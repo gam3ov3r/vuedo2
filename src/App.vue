16434cea4d78eec2864f1ee55a3b0f92930a1a4f
gam3ov3r <reklam@proclaim.se>
2018-01-20 00:36:04 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h1>{{ title }}</h1>
+		<ninjas></ninjas>
+	</div>
+</template>
+
+<script>
+import Ninjas from './Ninjas.vue'	
+	
+export default {
+
+components: {
+	'ninjas': Ninjas
+},
+  data () {
+    return {
+		title: 'Vuedo 2'
+    }
+  }
+}
+</script>
+
+<style scoped>
+	h1 {
+		color: red;
+	}
+	
+</style>
